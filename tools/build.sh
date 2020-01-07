f20b0895b9f52f3ddccba88643a675df0f633efd
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-07 15:50:10 +0800

    change namespace to shuzhi-amd64

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+NAMESPACE=("shuzhi-amd64")
+for i in ${NAMESPACE[*]}
+do
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/mllib_components:$1 -f docker/docker_yanqing/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/mllib_components_stream:$1 -f docker/stream_yanqing/Dockerfile .
 
-set -e
-
-VERSION=1.0.0
-
-. docker/build.sh $VERSION $@
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/mllib_components:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/mllib_components_stream:$1
+done
